376750cd640a90204c66f335594e43def7f7d3e2
lluisemper <lluisemper@gmail.com>
2020-05-05 13:14:49 +0200

    feat: mock data saved

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,11 +3,10 @@ import ApiService from '../ApiService';
 
 const Dashboard = () => {
 
-  ApiService.getCompanyData(true);
-
   return(
     <div>
       <h2>empty dashboard</h2>
+      
     </div>
   );
 }
--- src/components/Valuation.js
+++ src/components/Valuation.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import ApiService from '../ApiService';
 
-const Valuation = () => (
-  <div>
-    <p>fooo</p>
-  </div>
-);
+const Valuation = () => {
 
+  const companySearch = () => {
+    ApiService.getCompanyData('BABA').then(data => console.log("data", data) || data)
+  }
+  
+  return(
+    <div>
+      <p>fooo</p>
+      <button className="search-button" type="submit"
+                  onClick={() => companySearch()}>Search</button>
+    </div>
+  );
+}
 export default Valuation;
