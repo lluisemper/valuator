d305b1fc092ae99122d0c779115defd1f10bbdbb
lluisemper <lluisemper@gmail.com>
2020-05-08 18:47:38 +0200

    feat:header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,9 @@ import React from 'react';
 import '../App.css';
 
 const Header = () => (
-  <nav className="navbar">foooo
+  <nav className="navbar">
+    <h1>Finance Education</h1>
+    <img className="logo" src="./logo.png" alt="logo" height="64" width="128"/>
   </nav>
 );
 
