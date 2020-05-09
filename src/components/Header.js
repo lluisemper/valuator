23e903b80721a3bfe5b116c2fd70db206fc080b1
lluisemper <lluisemper@gmail.com>
2020-05-09 22:31:28 +0200

    feat: section container middle

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import '../App.css';
 const Header = () => (
   <nav className="navbar">
     <h1>Finance Education</h1>
-    <img className="logo" src="./logo.png" alt="logo" height="64" width="128"/>
+    <img className="logo" src="./logo.png" alt="logo"/>
   </nav>
 );
 
