28a8be4065717435a8f7b40c5ea0b38ee21d8998
lluisemper <lluisemper@gmail.com>
2020-05-13 23:52:44 +0200

    feat: link dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import '../App.css';
+import { Link } from 'react-router-dom';
+
 
 const Dashboard = () => {
 
   return(
     <div className="dashboard">
       <section className="container">
-        
+        <h4>~It's only when the tide goes out that you discover who's been swimming naked.</h4>
+        <div className="link-dashboard">
+          <Link to="/valuate" className="link-text">
+            Start Learning!
+          </Link>
+        </div>
       </section>
       
     </div>
